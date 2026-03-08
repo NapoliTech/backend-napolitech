a697b7756635967385d2eae1071e540cdfe4cf63
AlejandroFadim <alejandro.fadim@sptech.school>
2026-03-08 13:11:27 -0300

    o usuario marcos não é mais cadastrado quando a aplicação roda, ele estava dando erro de unique code

--- src/main/java/com/pizzaria/backendpizzaria/BackendPizzariaApplication.java
+++ src/main/java/com/pizzaria/backendpizzaria/BackendPizzariaApplication.java
@@ -1,23 +1,12 @@
 package com.pizzaria.backendpizzaria;
 
-import com.pizzaria.backendpizzaria.mock.DadosMock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendPizzariaApplication implements CommandLineRunner {
-
-    @Autowired
-    private DadosMock dadosMock;
+public class BackendPizzariaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendPizzariaApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        dadosMock.carregarDados();
     }
 }
